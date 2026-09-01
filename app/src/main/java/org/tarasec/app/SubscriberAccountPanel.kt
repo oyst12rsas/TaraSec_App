e61cb1178bd4cdcc196c9d30733791dc31c03130
oyst12rsas <oyst12rsas@gmail.com>
2026-09-01 06:15:29 +0200

    Fix subscriber account panel layout import

--- app/src/main/java/org/tarasec/app/SubscriberAccountPanel.kt
+++ app/src/main/java/org/tarasec/app/SubscriberAccountPanel.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.weight
 import androidx.compose.material3.Button
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.MaterialTheme
