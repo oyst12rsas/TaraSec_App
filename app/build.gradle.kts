1f2d527b14f5f838375920f5d6bfa9eda3407d9f
oyst12rsas <oyst12rsas@gmail.com>
2026-09-03 09:24:44 +0200

    Version global subscriber build as 0.2.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "org.tarasec.app"
         minSdk = 26
         targetSdk = 37
-        versionCode = 1
-        versionName = "0.1.0"
+        versionCode = 2
+        versionName = "0.2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
