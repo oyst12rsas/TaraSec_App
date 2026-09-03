c51208ddbb5cb16ae2babe81b6fdd182a29f15e8
oyst12rsas <oyst12rsas@gmail.com>
2026-09-03 15:06:28 +0200

    Bump app version for hotspot status fix

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "org.tarasec.app"
         minSdk = 26
         targetSdk = 37
-        versionCode = 3
-        versionName = "0.3.0"
+        versionCode = 4
+        versionName = "0.3.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
