9a61b29c8f05e604e0f3d5bc7c6733acd07311a0
oyst12rsas <oyst12rsas@gmail.com>
2026-09-03 15:46:57 +0200

    Bump app version for connectivity recheck

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "org.tarasec.app"
         minSdk = 26
         targetSdk = 37
-        versionCode = 5
-        versionName = "0.3.2"
+        versionCode = 6
+        versionName = "0.3.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
