364de9a6e482ecc8362c53cb884f45f647512e75
oyst12rsas <oyst12rsas@gmail.com>
2026-09-03 18:09:12 +0200

    Bump app version for balance formatting

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "org.tarasec.app"
         minSdk = 26
         targetSdk = 37
-        versionCode = 6
-        versionName = "0.3.3"
+        versionCode = 7
+        versionName = "0.3.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
