f319720827584cecac05c15dce7f36bdba125ebb
Øystein <oyst12rsas@gmail.com>
2026-09-14 12:12:47 +0200

    Restore debug APK copy task

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,3 +3,13 @@ plugins {
     id("org.jetbrains.kotlin.android") version "2.3.21" apply false
     id("org.jetbrains.kotlin.plugin.compose") version "2.3.21" apply false
 }
+
+
+tasks.register<org.gradle.api.tasks.Copy>("copyDebugApkToHome") {
+    group = "build"
+    description = "Copies the debug APK to the user home directory"
+    dependsOn(":app:assembleDebug")
+    from("app/build/outputs/apk/debug/app-debug.apk")
+    into(System.getProperty("user.home"))
+    rename { "TaraSec-debug.apk" }
+}
