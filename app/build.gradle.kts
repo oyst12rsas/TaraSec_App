6b682c79aacd05472c46522d4d4276fab2e16a08
oyst12rsas <oyst12rsas@gmail.com>
2026-09-16 17:06:01 +0200

    Add in-app GitHub contributor verification
    * Add GitHub contributor verification client
    
    * Add GitHub contributor verification panel
    
    * Show GitHub verification in contributor workspace
    
    * Bump app to 0.4.1
    
    * Allow replacing saved GitHub challenge
    
    * Clear replaced GitHub challenge

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,8 +20,8 @@ android {
         applicationId = "org.tarasec.app"
         minSdk = 26
         targetSdk = 37
-        versionCode = 13
-        versionName = "0.4.0"
+        versionCode = 14
+        versionName = "0.4.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
