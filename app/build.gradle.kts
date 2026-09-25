635fb99a3fbc14b2586e3f84843b82a66e911465
oyst12rsas <oyst12rsas@gmail.com>
2026-09-25 14:25:58 +0200

    Bump TaraSec to 0.4.3 for Coach test update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,8 +20,8 @@ android {
         applicationId = "org.tarasec.app"
         minSdk = 26
         targetSdk = 37
-        versionCode = 15
-        versionName = "0.4.2"
+        versionCode = 16
+        versionName = "0.4.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
